dd3aba9cdd42877be2df1fa8174608efac55b031
mehedibkash <mehedi.dsi@bkash.com>
2022-02-25 01:03:36 +0600

    Morning-off page design completed with check and close button

--- app/src/main/java/com/kata_chamooch/ui/morning/MorningOffFragment.kt
+++ app/src/main/java/com/kata_chamooch/ui/morning/MorningOffFragment.kt
@@ -14,13 +14,46 @@ class MorningOffFragment : Fragment() {
     private val binding get() = _binding!!
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentMorningOffBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-        return root
+
+        handleViewClick()
+        return binding.root
+    }
+
+    private fun handleViewClick() {
+        binding.launchCheckImg.setOnClickListener {
+            it.isSelected = !it.isSelected
+            binding.launchCrossImg.isSelected = false;
+        }
+
+        binding.launchCrossImg.setOnClickListener {
+            it.isSelected = !it.isSelected
+            binding.launchCheckImg.isSelected = false;
+        }
+
+        binding.dinnerCheckImg.setOnClickListener {
+            it.isSelected = !it.isSelected
+            binding.dinnerCrossImg.isSelected = false;
+        }
+
+        binding.dinnerCrossImg.setOnClickListener {
+            it.isSelected = !it.isSelected
+            binding.dinnerCheckImg.isSelected = false;
+        }
+
+        binding.snacksCheckImg.setOnClickListener {
+            it.isSelected = !it.isSelected
+            binding.snacksCrossImg.isSelected = false;
+        }
+
+        binding.snacksCrossImg.setOnClickListener {
+            it.isSelected = !it.isSelected
+            binding.snacksCheckImg.isSelected = false;
+        }
     }
 
     override fun onDestroyView() {
